f3d8a51730ffad486b8521c5e59c5337694d5328
Allan1418 <allan@PC-Allan>
2024-03-13 17:01:47 -0600

    primer commit

---
+++ src/main/java/com/prolearn/ProlearnApplication.java
@@ -0,0 +1,13 @@
+package com.prolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prolearn/ProlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
