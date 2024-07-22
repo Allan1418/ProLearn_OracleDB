620048371856892cfcd114528b29c7d505b38897
Allan1418 <allannb02@gmail.com>
2024-07-22 14:46:50 -0600

    primera parte de cp de de capitulo hijo para buscar por id

--- src/main/java/com/prolearn/dao/CapituloHijoDao.java
+++ src/main/java/com/prolearn/dao/CapituloHijoDao.java
@@ -3,7 +3,12 @@ package com.prolearn.dao;
 
 import com.prolearn.domain.CapituloHijo;
 import java.util.List;
+import java.util.function.Function;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.data.repository.query.Param;
 
 
 public interface CapituloHijoDao extends JpaRepository<CapituloHijo, Long> {
@@ -11,5 +16,11 @@ public interface CapituloHijoDao extends JpaRepository<CapituloHijo, Long> {
     CapituloHijo findByIdAndCapituloPadreId(Long id, Long capituloPadreId);
     
     List<CapituloHijo> findAllByCapituloPadreId(Long capituloPadreId);
+
+    @Override
+    @Procedure(name = "PROLEARN.CH_FINDBYID_SP")
+    public <S extends CapituloHijo, R> R findBy(@Param("P_ID_CAPITULO_HIJO") Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction);
+    
+    
     
 }
