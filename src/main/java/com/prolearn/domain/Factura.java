3d442054592a8a9ff72a48ec886a3e14f439d5b0
rugamac <105762180+rugamac@users.noreply.github.com>
2024-08-19 19:57:32 -0600

    Modificacion facturas

--- src/main/java/com/prolearn/domain/Factura.java
+++ src/main/java/com/prolearn/domain/Factura.java
@@ -3,16 +3,57 @@ package com.prolearn.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.io.Serializable;
+import java.sql.Time;
 import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "FACTURA_TB")
+@NamedStoredProcedureQuery(
+    name = "SPFindXIdFT",
+    procedureName = "FACTURA_GET_BYID_SP",
+    parameters = {
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_ID_FACTURA", type = Long.class),
+        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "P_RESULTADO", type = void.class)
+    },
+    resultClasses = { Factura.class } 
+)
+@NamedStoredProcedureQuery(
+    name = "SPFindAllXIdCursoFT",
+    procedureName = "FACTURA_GETALL_BY_CURSO_SP",
+    parameters = {
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_ID_CURSO", type = Long.class),
+        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "P_FACTURAS", type = void.class)
+    },
+    resultClasses = { Factura.class } 
+)
+@NamedStoredProcedureQuery(
+    name = "SPUpsertFT",
+    procedureName = "FACTURA_UPSERT_SP",
+    parameters = {
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_ID_FACTURA", type = Long.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_MONTO", type = Double.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_FECHA_PAGO", type = Time.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_FECHA_EXPIRACION", type = Time.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_LAST_UPDATE_BY", type = String.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_LAST_UPDATE_DATE", type = Date.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_CREATED_BY", type = String.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_CREATION_DATE", type = Date.class),
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_ACCION", type = String.class)
+    }
+)
+@NamedStoredProcedureQuery(
+    name = "SPDeleteFT",
+    procedureName = "FACTURA_DELETE_SP",
+    parameters = {
+        @StoredProcedureParameter(mode = ParameterMode.IN, name = "P_ID_FACTURA", type = Long.class),
+    }
+)
+
 public class Factura implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "FACTURA_TB_ID_FACTURA_PK")
     private Long id;
 
